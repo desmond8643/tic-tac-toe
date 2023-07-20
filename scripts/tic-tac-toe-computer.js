f60e045927b30feeaf94b372eb555c4dc6d435d5
Desmond <128795516+desmond8643@users.noreply.github.com>
2023-07-20 16:27:45 +0800

    Update tic-tac-toe-computer.js

--- scripts/tic-tac-toe-computer.js
+++ scripts/tic-tac-toe-computer.js
@@ -6,28 +6,31 @@ const symbol = {
 
 const description = document.querySelector('.js-description');
 
-const board = {
-    grid1: document.getElementById('js-grid-1'),
-    grid2: document.getElementById('js-grid-2'),
-    grid3: document.getElementById('js-grid-3'),
-    grid4: document.getElementById('js-grid-4'),
-    grid5: document.getElementById('js-grid-5'),
-    grid6: document.getElementById('js-grid-6'),
-    grid7: document.getElementById('js-grid-7'),
-    grid8: document.getElementById('js-grid-8'),
-    grid9: document.getElementById('js-grid-9')
-}
+//use array to store the dom 
+const board = 
+[
+    document.getElementById('js-grid-1'),
+    document.getElementById('js-grid-2'),
+    document.getElementById('js-grid-3'),
+    document.getElementById('js-grid-4'),
+    document.getElementById('js-grid-5'),
+    document.getElementById('js-grid-6'),
+    document.getElementById('js-grid-7'),
+    document.getElementById('js-grid-8'),
+    document.getElementById('js-grid-9')
+]
 
 let playerTurn = 'Player 1';
-    
+let currentGrid = [];
+
 /* activate event listeners and update description at the beginning*/
 startGame(); 
 
 function startGame() {
-    for (const grid in board) {
-        updateBoard(board[grid]);
-    }
-    
+    board.forEach(grid => {
+        updateBoard(grid)
+    })
+
     updateDescription();
 }
 
@@ -36,6 +39,8 @@ function updateBoard(grid) {
         if (grid.value === '') {
             if (playerTurn === 'Player 1') {
                 grid.value = symbol[playerTurn];
+                getCurrentGrid();
+
                 if (!checkWin(symbol[playerTurn])) {
                     playerTurn = 'Player 2';
                     updateDescription();
@@ -49,22 +54,42 @@ function updateBoard(grid) {
     grid.addEventListener('click', listener);
 }
 
+function getCurrentGrid() {
+    currentGrid = [];
+    board.forEach(grid => {
+        currentGrid.push(grid.value);
+    })
+    console.log(currentGrid)
+}
+
 function computerMove() {
     let blankGrid = [];
-    //find blank grid and put it in an array
-    for (const grid in board) {
-        if (board[grid].value === '') {
-            blankGrid.push(board[grid]);
+
+    board.forEach(grid => {
+        if (grid.value === '') {
+            blankGrid.push(grid)
         }
+    })
+
+    if (board[4].value === '') {
+        board[4].value = symbol[playerTurn];
+    } else {
+        randomGenerateComputerMove(blankGrid)
     }
-    //random generate a number range from the array size
-    let getRandomMove = Math.floor(Math.random() * blankGrid.length);
-    //get computer move by using that random generated number
-    blankGrid[getRandomMove].value = symbol[playerTurn];
+
+    getCurrentGrid();
+
     if (!checkWin(symbol[playerTurn])) {
         playerTurn = 'Player 1';
         updateDescription();
-    }    
+    }
+}
+
+function randomGenerateComputerMove(blankGrid) {
+    //random generate a number range from the array size
+    let getRandomMove = Math.floor(Math.random() * blankGrid.length);
+    //get computer move by using that random generated number
+        blankGrid[getRandomMove].value = symbol[playerTurn];
 }
 
 function updateDescription() {
@@ -72,78 +97,36 @@ function updateDescription() {
 }
 
 function checkWin(player) {
-    if (board.grid1.value === player && board.grid2.value === player && board.grid3.value === player) { /* horizontal wins */
-        playerWin();
-        return true;
-    } 
-    if (board.grid4.value === player && board.grid5.value === player && board.grid6.value === player) {
-        playerWin();
-        return true;
-    } 
-    if (board.grid7.value === player && board.grid8.value === player && board.grid9.value === player) {
-        playerWin();
-        return true;
-    } 
-    if (board.grid1.value === player && board.grid4.value === player && board.grid7.value === player) { /* vertical wins */
-        playerWin();
-        return true;
-    } 
-    if (board.grid2.value === player && board.grid5.value === player && board.grid8.value === player) { 
-        playerWin();
-        return true;
-    } 
-    if (board.grid3.value === player && board.grid6.value === player && board.grid9.value === player) { 
-        playerWin();
-        return true;
-    } 
-    if (board.grid1.value === player && board.grid5.value === player && board.grid9.value === player) { /* diagonal wins */
-        playerWin();
-        return true;
-    } 
-    if (board.grid3.value === player && board.grid5.value === player && board.grid7.value === player) { 
-        playerWin();
-        return true;
+    if (winCombinations(0, 1, 2, player) || winCombinations(3, 4, 5, player) || winCombinations(6, 7, 8, player) || 
+        winCombinations(0, 3, 6, player) || winCombinations(1, 4, 7, player) || winCombinations(2, 5, 8, player) ||
+        winCombinations(0, 4, 8, player) || winCombinations(2, 4, 6, player)) {
+            playerWin()
+            return true;
     }
-    
+
     if (checkTie()) {
         playerTie();
         return true;
-
     } else {
-        return false; 
+        return false;
     }
-    
+}
+
+function winCombinations(one, two, three, player) {
+    return currentGrid[one] === player && currentGrid[two] === player && currentGrid[three] === player 
 }
 
 function playerWin() {
     description.innerHTML = `${playerTurn} wins!
     <button class="js-play-again-button play-again-button">Play again</button>`;
-    for (const grid in board) {
-        disableBoard(board[grid]);
-    }
+    board.forEach(grid => {
+        disableBoard(grid)
+    })
 
     document.querySelector('.js-play-again-button').addEventListener('click', () => {
-        for (const grid in board) {
-            resetBoard(board[grid]);
-        }
-        updateDescription();
-        if (playerTurn === 'Player 2') {
-            computerMove();
-        }
-    });
-  }
-
-function playerTie() {
-    description.innerHTML = `It's a tie match!
-    <button class="js-play-again-button play-again-button">Play again</button>`;
-    for (const grid in board) {
-        disableBoard(board[grid]);
-    }
-
-    document.querySelector('.js-play-again-button').addEventListener('click', () => {
-        for (const grid in board) {
-            resetBoard(board[grid]);
-        }
+        board.forEach(grid => {
+            resetBoard(grid)
+        })
         updateDescription();
         if (playerTurn === 'Player 2') {
             computerMove();
@@ -151,21 +134,38 @@ function playerTie() {
     });
 }
 
-  function disableBoard(grid) {
+function playerTie() {
+    description.innerHTML = `It's a tie match!
+    <button class="js-play-again-button play-again-button">Play again</button>`;
+    board2.forEach(grid => {
+        disableBoard(grid)
+    })
+
+    document.querySelector('.js-play-again-button').addEventListener('click', () => {
+        board.forEach(grid => {
+            resetBoard(grid)
+        })
+        updateDescription();
+        if (playerTurn === 'Player 2') {
+            computerMove();
+        }
+    });
+}
+
+function disableBoard(grid) {
     grid.disabled = true;
   }
 
-  function resetBoard(grid) {
+function resetBoard(grid) {
     grid.value = '';
     grid.disabled = false;
   }
 
-  function checkTie() {
-    for (const grid in board) {
-        if (board[grid].value === '') {
+function checkTie() {
+    for (let i = 0; i < board.length; i++) {
+        if (board[i].value === '') {
             return false;
         }
     }
     return true;
-
-  }
+}
